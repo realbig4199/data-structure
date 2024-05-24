fd3a90a8c9c71dedb639d9bb13aeaeb4f610b64d
realbig4199 <realbig4199@gmail.com>
2024-05-25 07:40:26 +0900

    덱 구현

---
+++ 덱/Deque.js
@@ -0,0 +1,33 @@
+const { DoublyLinkedList } = require("../큐/DoublyLinkedList");
+
+class Deque {
+  constructor() {
+    this.list = new DoublyLinkedList();
+  }
+
+  printAll() {
+    this.list.printAll();
+  }
+
+  addFirst(data) {
+    this.list.insertAt(0, data);
+  }
+
+  removeFirst() {
+    return this.list.deleteAt(0);
+  }
+
+  addLast(data) {
+    this.list.insertAt(this.list.count, data);
+  }
+
+  removeLast() {
+    return this.list.deleteLast();
+  }
+
+  isEmpty() {
+    return this.list.count === 0;
+  }
+}
+
+module.exports = Deque;
